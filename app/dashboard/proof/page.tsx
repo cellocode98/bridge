175e01277c5af2e21a125eb07e29d6483e059776
Liam <cellocode98@gmail.com>
2025-10-27 22:57:18 -0500

    minor fix to accomodate for build refer to previous commit for other changes

--- app/dashboard/proof/page.tsx
+++ app/dashboard/proof/page.tsx
@@ -3,9 +3,12 @@
 import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import ProofUploadPage from "@/components/ProofPage";
+import { Suspense } from "react";
 
 export default function ProofPage() {
   return(
+    <Suspense>
     <ProofUploadPage/>
+    </Suspense>
   );
 }
